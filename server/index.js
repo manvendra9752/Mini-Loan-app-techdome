3a5eb75008a67fc059f8ecba11df9349c2f7ba65
manvendra9752 <manvendrachouhan11@gmail.com>
2024-10-24 07:42:14 +0530

    fix server

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/user", require("./routes/user"));
 app.use("/api/loan", require("./routes/loan"));
